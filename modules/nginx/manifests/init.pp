c403c90628d0dbbd9a7853353e0025425863852a
wulff <wulff@ratatosk.net>
2012-05-21 17:45:06 +0200

    added server for munin

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -12,8 +12,6 @@ class nginx {
   }
 
   file {
-#    '/var/cache/nginx/':
-#      ensure => directory;
     '/var/www':
       ensure => directory,
       mode => 775,
@@ -30,38 +28,9 @@ class nginx {
       source => 'puppet:///modules/nginx/nginx.conf',
       require => Package['nginx'],
       notify => Service['nginx'];
-
-#    '/etc/nginx/sites-available/admin.conf':
-#      source => 'puppet:///modules/nginx/admin.conf',
-#      require => Package['nginx-light'];
-
-#    '/etc/nginx/conf.d/fastcgi.conf':
-#      source => 'puppet:///modules/nginx/fastcgi.conf',
-#      require => Package['nginx-light'],
-#      notify => Service['nginx'];
-#    '/etc/nginx/conf.d/map_cache.conf':
-#      source => 'puppet:///modules/nginx/map_cache.conf',
-#      require => Package['nginx-light'],
-#      notify => Service['nginx'];
-#    '/etc/nginx/conf.d/microcache.conf':
-#      source => 'puppet:///modules/nginx/microcache.conf',
-#      require => Package['nginx-light'],
-#      notify => Service['nginx'];
-#    '/etc/nginx/conf.d/upstream.conf':
-#      source => 'puppet:///modules/nginx/upstream.conf',
-#      require => Package['nginx-light'],
-#      notify => Service['nginx'];
-#    '/etc/nginx/sites-available/drupal.conf':
-#      source => 'puppet:///modules/nginx/drupal.conf',
-#      require => Package['nginx-light'];
-#    '/etc/nginx/htpasswd':
-#      source => 'puppet:///modules/nginx/htpasswd',
-#      require => Package['nginx-light'];
-
-#    '/etc/nginx/sites-enabled/default':
-#      ensure => absent,
-#      require => Package['nginx'],
-#      notify => Service['nginx'];
+    '/etc/nginx/htpasswd':
+      source => 'puppet:///modules/nginx/htpasswd',
+      require => Package['nginx'];
 
     '/etc/nginx/sites-available/default':
       source => 'puppet:///modules/nginx/default.conf',
@@ -91,25 +60,5 @@ class nginx {
     '/etc/nginx/sites-enabled/ratatosk.net.conf':
       ensure => '/etc/nginx/sites-available/ratatosk.net.conf',
       require => File['/etc/nginx/sites-available/ratatosk.net.conf'];
-
-#    '/etc/nginx/sites-enabled/000-default':
-#      ensure => '/etc/nginx/sites-available/default',
-#     require => File['/etc/nginx/sites-available/default'];
-
-#    '/etc/nginx/sites-available/cdn.conf':
-#      source => 'puppet:///modules/nginx/cdn.conf',
-#      require => Package['nginx-light'],
-#      notify => Service['nginx'];
-#    '/etc/nginx/sites-enabled/cdn.conf':
-#      ensure => '/etc/nginx/sites-available/cdn.conf',
-#      require => File['/etc/nginx/sites-available/cdn.conf'];
-
-#    '/etc/nginx/sites-available/drupal7.conf':
-#      source => 'puppet:///modules/nginx/drupal7.conf',
-#      require => Package['nginx-light'],
-#      notify => Service['nginx'];
-#    '/etc/nginx/sites-enabled/drupal7.conf':
-#      ensure => '/etc/nginx/sites-available/drupal7.conf',
-#      require => File['/etc/nginx/sites-available/drupal7.conf'];
   }
 }
--- modules/ratatosk/files/publish.sh
+++ modules/ratatosk/files/publish.sh
@@ -2,4 +2,4 @@
 
 # TODO: use argument for site name
 
-rsync -r --delete /home/wulff/jekyll/psyke.org/html/ /home/wulff/www/psyke.org/
+rsync -a --delete /home/wulff/jekyll/psyke.org/html/ /home/wulff/www/psyke.org/
